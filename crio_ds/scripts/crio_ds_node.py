e43328e98a85525cc5e99e0b10668be050ff14f3
Ryan Sjostrand <rsjostrand@protonmail.com>
2016-06-05 15:42:24 -0500

    Reflect changes to lib_crio, and fixes

--- crio_ds/scripts/crio_ds_node.py
+++ crio_ds/scripts/crio_ds_node.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python
 import rospy
+import roslog
 from crio.communication import DS
 from crio.communication import DSException
-from crio_ds.msg import DriverStation
-
-def callback(data):
-    rospy.loginfo(type(data))
-    ds.state_lock.acquire()
-    ds.state.enabled = data.enabled
-    ds.state_lock.release()
+from crio_ds.msg import DriverStationState
+from std_msgs.msg import Bool
 
 
-def listener():
-    rospy.init_node('ds')
-    team = rospy.get_param("team", 3081)
-    rospy.loginfo("Team: %i", team)
+class CrioDSNode:
+    def __init__(self):
+        rospy.init_node('ds')
+        team = rospy.get_param("team", 3081)
+        rospy.loginfo("Team: %i", team)
+        try:
+            self.ds = DS(team)
+        except DSException as e:
+            rospy.logfatal("Could not start the Driver Station")
+            return
 
-    ds = None
-    try:
-        ds = DS(team)
-    except DSException as e:
-        #rospy.logfatal()
-        #print e
-        return
+        self.pub = rospy.Publisher("/ds_state", DriverStationState)
+        rospy.Subscriber("/ds_enabled", Bool,  callback=CrioDSNode.callback, callback_args=(self,))
 
-    rospy.Subscriber("/crio", DriverStation, callback)
-    rospy.loginfo("Spinning")
-    rate = rospy.Rate(30)
-    while not rospy.is_shutdown():
-        rate.sleep()
+    def run(self):
+        rospy.loginfo("Spinning")
+        rate = rospy.Rate(30)
+        while not rospy.is_shutdown():
+            self.ds.state_lock.acquire()
+            msg = DriverStationState()
+            msg.packet = ds.packet_number
+            msg.enabled = ds.state.enabled
+            self.ds.state_lock.release()
+            rate.sleep()
+        ds.stop()
 
-    ds.stop()
+    def callback(self):
+        pass
+
 if __name__ == "__main__":
-    listener()
-#Report error mesage, and use ros param
-#ds = DS(3081)
+    roslog.init("DS")
+    node = CrioDSNode()
+    node.run()
