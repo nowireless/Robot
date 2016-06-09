de33c6413c9456a19b92d5e694c3b3013cd89c66
Ryan Sjostrand <rsjostrand@protonmail.com>
2016-06-09 14:27:26 -0500

    Start of Networktables support for the driver

--- crio_driver/src/crio_driver/crio_driver_node.cpp
+++ crio_driver/src/crio_driver/crio_driver_node.cpp
@@ -1,8 +1,55 @@
 #include <ros/ros.h>
+
+#include <atomic>
+#include <ntcore.h>
+#include <tables/ITableListener.h>
+#include <networktables/NetworkTable.h>
+
 #include <controller_manager/controller_manager.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <hardware_interface/joint_state_interface.h>
 #include <hardware_interface/robot_hw.h>
+
+
+/*
+ * Log callback to bridge ros and Networktable logging
+ */
+void nt_log_callback(unsigned int level, const char* file, unsigned int line, const char* msg) {
+	switch (level) {
+		case NT_LOG_CRITICAL:
+			ROS_FATAL("NT: %s (%s:%d)", msg, basename(file), line);
+			break;
+		case NT_LOG_ERROR:
+			ROS_ERROR("NT: %s (%s:%d)", msg, basename(file), line);
+			break;
+		case NT_LOG_WARNING:
+			ROS_WARN("NT: %s (%s:%d)", msg, basename(file), line);
+			break;
+		case NT_LOG_INFO:
+			ROS_INFO("NT: %s (%s:%d)", msg, basename(file), line);
+			break;
+		case NT_LOG_DEBUG:
+		case NT_LOG_DEBUG1:
+		case NT_LOG_DEBUG2:
+		case NT_LOG_DEBUG3:
+		case NT_LOG_DEBUG4:
+			ROS_DEBUG("NT: %s (%s:%d)", msg, basename(file), line);
+			break;
+		default:
+			break;
+	}
+}
+
+atomic<bool> CONNECTED;
+void connection_callback(unsigned int uid, bool connected, const nt::ConnectionInfo& conn) {
+	CONNECTED.store(connected);
+
+	if(connected) {
+		ROS_INFO("NT Connected");
+	} else {
+		ROS_INFO("NT Disconnected");
+	}
+}
 
 class CRIO : public hardware_interface::RobotHW {
 public:
@@ -64,9 +111,24 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "crio_driver");
 	ros::NodeHandle nh;
 	
+	//Initialize Networktables
+	string ip;
+	n.param<string>("crio_ip", ip ,"127.0.0.1");
+	ROS_INFO("NT server IP %s", ip.c_str());
+	ROS_INFO("Starting NT");
+	nt::AddConnectionListener(connection_callback,true);
+	nt::SetLogger(nt_log_callback, NT_LOG_INFO);
+	NetworkTable::SetClientMode();
+	NetworkTable::SetIPAddress(llvm::StringRef(ip));
+
+	//Get bridge table
+	shared_ptr<NetworkTable> bridge = NetworkTable::GetTable("bridge");
+	
+	ros::Publisher pubConnected = n.advertise<std_msgs::Bool>("connected", 100);
+
 	CRIO robot;
 	controller_manager::ControllerManager cm(&robot, nh);
-	
+
 	ros::Rate rate(1.0/robot.GetPeriod().toSec());
 	ros::AsyncSpinner spinner(1);
 	spinner.start();
